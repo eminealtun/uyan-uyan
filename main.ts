e4f7d67fd9af8b24b5c8a36154dd1d30a309f78b
eminealtun <85133216+eminealtun@users.noreply.github.com>
2021-06-01 00:04:04 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.showIcon(IconNames.Asleep)
+//  "Duraklat" üzerinde ve altında çalışan kod blokları arasındaki bekleme süresini ifade eder
+basic.pause(1000)
+basic.showIcon(IconNames.Surprised)
+basic.pause(1000)
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
